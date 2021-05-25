ba7b5106873a85bd65073191b703ebe7a9a2e047
en-chang <enchangcs@gmail.com>
2021-05-25 17:08:47 -0500

    Timer beeps on completion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Timer from './components/Timer/Timer';
-import Button from './components/Button/Button';
 import Beep from './components/Beep/Beep'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1>Hello World!</h1>
+      <h1>Hello Timer!</h1>
       <Timer></Timer>
-      <br></br>
-      <Button></Button>
       <br></br>
       <Beep></Beep>
     </div>
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Countdown from 'react-countdown';
+import Button from '../Button/Button.js';
 import './Timer.css';
 
+const a=new AudioContext() // browsers limit the number of concurrent audio contexts, so you better re-use'em
+
+function beep(vol, freq, duration){
+  var v=a.createOscillator()
+  var u=a.createGain()
+  v.connect(u)
+  v.frequency.value=freq
+  v.type="square"
+  u.connect(a.destination)
+  u.gain.value=vol*0.01
+  v.start(a.currentTime)
+  v.stop(a.currentTime+duration*0.001)
+}
+
 class Timer extends React.Component {
+  completed() {
+    beep(20,100,30);
+  }
   render() {
     return (
-      <Countdown className='timer' date={Date.now() + 5000}>
-        {/* Sound on completion */}
-      </Countdown>
+      <div>
+        <Countdown className='timer' date={Date.now() + 5000} onComplete={this.completed}></Countdown>
+        <br></br>
+        <Button></Button>
+      </div>
     );
   }
 }
